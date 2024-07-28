e11760b04c78de3311f3b1c3fcbe0d4961331b62
abbasnosrat <abbasnosrat@gmail.com>
2024-07-28 14:01:31 +0330

    code cleanup

--- api.py
+++ api.py
@@ -14,7 +14,6 @@ async def home_page():
 @app.post("/upload-data/")
 async def upload_csv(file: Annotated[UploadFile, File(description="Some description")] = None, Budget:float = None):
     global global_csv_data
-    # Read the uploaded CSV file into a pandas DataFrame
     global budget
     if Budget != None:
         budget = Budget
@@ -28,7 +27,6 @@ async def upload_csv(file: Annotated[UploadFile, File(description="Some descript
     elif file !=None and Budget ==None:
         return {"message": "using default budget"}
     else: return {"message": "using default values for budget and dataset"}
-    # Store the DataFrame in the global variable
     
     
 
@@ -46,10 +44,6 @@ async def process_csv():
 
     solution = problem.solve()
     result= pd.concat([products,pd.DataFrame({"optimal_quantity": solution["quantities"]})],axis=1)
-    # Example operation: Get the number of rows and columns in the CSV
     
     return result.to_json()
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
